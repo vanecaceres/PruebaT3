9f56d8f9b8fc835b01ff13ddeb7540b09e0571a9
vanecaceres <jv.caceres10@uniandes.edu.co>
2018-10-28 16:06:21 -0500

    DOCS

--- PCR.py
+++ PCR.py
@@ -1,2 +1,8 @@
 
 file = open("WDBC.dat", "r")
+or linea in file:
+    linea = linea.split(‘,’)
+    print ‘Persona: ‘
+    print ‘————‘
+    print ‘Nombre: ‘ + linea[0] + ‘, Apellidos: ‘ + linea[1] + ‘, Edad: ‘ + linea[2]
+    print ‘\n’
