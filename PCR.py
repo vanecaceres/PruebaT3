fd73f0552a4da7c32e36977faa52185d7706e521
vanecaceres <jv.caceres10@uniandes.edu.co>
2018-10-29 08:47:21 -0500

    Colum

--- PCR.py
+++ PCR.py
@@ -1,12 +1,12 @@
 import numpy as np
 import csv
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+from io import StringIO
 
-import urllib.request
- 
-headers = {}
-headers['User-Agent'] = "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:48.0) Gecko/20100101 Firefox/48.0"
- 
-req = urllib.request.Request('http://ftp.cs.wisc.edu/math-prog/cpo-dataset/machine-learn/cancer/WDBC/WDBC.dat', headers = headers)
-html = urllib.request.urlopen(req).read()
-print(html)
+datos = np.genfromtxt("WDBC.dat", delimiter = ",",unpack = True,  usecols = (0,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31))
+#print(datos)
+
+columnas = []
+for i in range(len(datos[:,0])):
+    columnas.append(i)
+    columnas[i] = datos[:,i]
