ba30dbb28ab38dd2dd87824ac58dd465b441dbca
vanecaceres <jv.caceres10@uniandes.edu.co>
2018-10-28 23:41:08 -0500

    h

--- PCR.py
+++ PCR.py
@@ -3,18 +3,7 @@ import csv
 import matplotlib.pyplot as plt
 from io import StringIO
 
-datos = open("WDBC.dat", "r")
 
-columna = np.array([])
-for i in range(datos):
-	columna[i] =datos[:,i]
+datos = open("WDBC.dat", "r", usecols = (1,2,3,4), skip_header = 0, delimiter = ",")
 
-
-arreglo = np.ones((4,4))
-resultado = 0
-for i in range(4):
-    for j in range(4):
-        for k in range(len(datos[:,0])-1):
-            resultado = ((datos[k,i] - media[i])*(datos[k,j]-media[j]))/tamano
-            arreglo[i,j] = resultado
-print (arreglo)
+print (datos)
