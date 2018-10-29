abe4eb049447e895588f5355a31b6e7b2e1e3086
vanecaceres <jv.caceres10@uniandes.edu.co>
2018-10-28 19:38:41 -0500

    DOCS

--- PCR.py
+++ PCR.py
@@ -6,3 +6,13 @@ or linea in file:
     print ‘————‘
     print ‘Nombre: ‘ + linea[0] + ‘, Apellidos: ‘ + linea[1] + ‘, Edad: ‘ + linea[2]
     print ‘\n’
+
+
+arreglo = np.ones((4,4))
+resultado = 0
+for i in range(4):
+    for j in range(4):
+        for k in range(len(datos[:,0])-1):
+            resultado = ((datos[k,i] - media[i])*(datos[k,j]-media[j]))/tamano
+            arreglo[i,j] = resultado
+print (arreglo)
