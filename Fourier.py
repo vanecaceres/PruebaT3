6f346df038ddf1a1dc64f8da8fc72845369bb20d
vanecaceres <jv.caceres10@uniandes.edu.co>
2018-10-29 18:26:20 -0500

    FourierDef

--- Fourier.py
+++ Fourier.py
@@ -1,28 +1,30 @@
 import numpy as np
 import csv
 import matplotlib.pyplot as plt
+from scipy.fftpack import ifft
+from scipy.interpolate import interp1d
 from io import StringIO
 
-signal = np.genfromtxt("signal.dat",delimiter = ",",unpack = True)
+signal = np.genfromtxt("signal.dat",delimiter = ",", dtype=None, comments='#',unpack = True)
 
-incompletos = np.genfromtxt("incompletos.dat",delimiter = ",",unpack = True)
+incompletos = np.genfromtxt("incompletos.dat",delimiter = ",",dtype=None, comments='#',unpack = True)
 
 filasIncompletos = []
 columnasIncompletos = []
 
-print(len(incompletos[1,:]),len(incompletos[:,1]))
+#print(len(incompletos[1,:]),len(incompletos[:,1]))
 columna1I = (incompletos[0,:])
 columna2I = (incompletos[1,:])
 
-x = np.linspace(-4,-4,100)
-#plt.plot(incompletos,signal)
+#Funcion que devuelve la transformada de fourier y el espectro de frecuencias
+def analisis_fourier(tiempo, variable, fc):
+#Numero de datos 
+	ndatos = len(variable) 
+#Creo un arreglo de ceros del tamaño que me entre para los numeros reales e imaginarios
+    	real = np.zeros(np.shape(variable))
+    	imaginario = np.zeros(np.shape(variable))
 
-
-for i in range(len(incompletos[:,1])):
-    for j in range(len(incompletos[1,:])):
-                   
-        filasIncompletos[i] = incompletos[i,:]
-        columnasIncompletos[j] = incompletos[:,j]
-        
-        filasSignal[i] = signal[i,:]
-        columnasSignal[j] = signal[:,j]
+for i in range(len(variable)):
+	imaginario[i]=0
+	real[i] =0
+	for j in range(len(variable)):
