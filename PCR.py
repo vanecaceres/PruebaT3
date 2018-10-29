115670180f5c0ef003098122bf52348aeba43bfa
vanecaceres <jv.caceres10@uniandes.edu.co>
2018-10-28 21:03:46 -0500

    DOCS1

--- PCR.py
+++ PCR.py
@@ -1,12 +1,16 @@
 
-file = open("WDBC.dat", "r")
-or linea in file:
+datos = open("WDBC.dat", "r")
+or linea in datos:
     linea = linea.split(‘,’)
     print ‘Persona: ‘
     print ‘————‘
     print ‘Nombre: ‘ + linea[0] + ‘, Apellidos: ‘ + linea[1] + ‘, Edad: ‘ + linea[2]
     print ‘\n’
 
+columna1 =datos[:,0]
+columna2 =datos[:,1]
+columna3 =datos[:,2]
+columna4 =datos[:,3]
 
 arreglo = np.ones((4,4))
 resultado = 0
