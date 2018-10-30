3d8a9708f0a80892f904c796728ac8b00f04d5e9
vanecaceres <jv.caceres10@uniandes.edu.co>
2018-10-29 21:30:00 -0500

    Im

--- Fourier.py
+++ Fourier.py
@@ -4,13 +4,13 @@ import matplotlib.pyplot as plt
 from scipy.fftpack import ifft
 from scipy.interpolate import interp1d
 from io import StringIO
-
+#Importo los datos de signal.dat
 signal = np.genfromtxt("signal.dat", dtype=None, comments="#", delimiter=",",unpack = False)
+#Importo los datos desde incompletos.dat
 incompletos = np.genfromtxt("incompletos.dat", dtype=None, comments="#", delimiter=",", unpack = False)
 
 filasIncompletos = []
 columnasIncompletos = []
-
 #print(len(incompletos[1,:]),len(incompletos[:,1]))
 columna1I = (incompletos[0,:])
 columna2I = (incompletos[1,:])
@@ -21,14 +21,16 @@ def analisis_fourier(tiempo, variable, fc):
     ndatos = len(variable) 
 #Creo un arreglo de ceros del tamaño que me entre para los numeros reales e imaginarios
     real = np.zeros(np.shape(variable))
+#Arreglo de ceros imaginarios
     imaginario = np.zeros(np.shape(variable))
-
+#Hago dos for que me recorran el tamaño que me entra por parametro
     for i in range(len(variable)):
         imaginario[i]=0
         real[i] =0
         for j in range(len(variable)):
-            #Me va a ir sumando los valores
+            #Me va a ir sumando los valores bk
             imaginario[i]= imaginario[i] + variable[j]*np.sin(-2*np.pi*i*j/ndatos)
+		#Me conformal la funcion de ak
             real[i] = real[i] + variable[j]*np.cos(-2*np.pi*i*j/ndatos)
 
     #Calculo las frecuencias
@@ -45,11 +47,11 @@ def analisis_fourier(tiempo, variable, fc):
     #Lleno mi vector de frecuencias con los valores
     frecuencias[0:n2] = np.arange(delta_frecuencia, fmaxima+delta_frecuencia, delta_frecuencia)
     frecuencias[n2:] = np.arange(-fmaxima, -delta_frecuencia+delta_frecuencia, delta_frecuencia)
-
+    #Hago dos arreglos de ceros de nuevo con el tamño de la variable que me entra por parametro para mis reales e imaginarios pero ahora infiltrados
     realfiltrado = np.zeros(np.shape(variable))
     imaginariofiltrado = np.zeros(np.shape(variable))
     
-    #Filtrar la senal
+    #Voy a comenzar a filtrar la señal que se encuentre entre +,- la frecuencia que me entra por restriccion
     for i in range(ndatos):
         if -fc < frecuencias[i] and frecuencias[i] < fc:
             # Valores para conservar porque estan en dentro de las condiciones
@@ -62,7 +64,7 @@ def analisis_fourier(tiempo, variable, fc):
 
     
     filtrado = ifft(realfiltrado + j*imaginariofiltrado)
-    # retornar las varialbes de interes
+    #Devuelvo los valores que necesito
     return real, imaginario, frecuencias, filtrado.real
 #filtro pasa bajos con frecuencia de corte fc = 1000Hz
 real, imaginario, frecuencias, filtrado = analisis_fourier(signal[:,0], signal[:,1], fc=1000)
@@ -86,6 +88,6 @@ plt.xlabel("Tiempo []")
 plt.ylabel("Amplitud")
 plt.grid()
 plt.savefig('CaceresNaranjoVanessa_filtrado.pdf')
-
+#Mensaje de que no se puede realizar la segunda transformada con incompletos
 print("No se puede hacer la transformada de los datos incompletos.dat porque los datos de la variable del tiempo no estan bien espaciados")
 
--- Fourier2D.py
+++ Fourier2D.py
@@ -3,3 +3,12 @@ import matplotlib.pyplot as plt
 import matplotlib.image as img
 from scipy import fftpack
 from scipy.interpolate import interp1d
+
+#Cargo la imagen del arbol
+arbol_numpy = img.imread("arbol(1).png")
+#Transformada de fourier en 2D
+arbol_fourier = fftpack.fft2(arbol_numpy)
+#Creo una imagen y la guardo
+plt.figure()
+plt.imshow(np.log(np.abs(arbol_fourier)))
+plt.savefig("CaceresNaranjoVanessa_FT2D.pdf")
